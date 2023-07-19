5078a5dab032c56b532a673146bf73af689075fd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-19 16:34:32 +0530

    created new activity to display tasks

---
+++ app/src/main/java/com/example/taskscheduler/DisplayTasks.kt
@@ -0,0 +1,11 @@
+package com.example.taskscheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayTasks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_tasks)
+    }
+}
