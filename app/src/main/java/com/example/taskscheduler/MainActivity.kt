1f5a3f9f7abe3fcc85a7f4de571bc5bff07d984f
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-19 20:08:45 +0530

    configure sign in options

--- app/src/main/java/com/example/taskscheduler/MainActivity.kt
+++ app/src/main/java/com/example/taskscheduler/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.taskscheduler
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taskscheduler.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -11,13 +13,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     lateinit var mGoogleSignInClient: GoogleSignInClient
-    val Req_Code:Int=123
-    val firebaseAuth= FirebaseAuth.getInstance()
+    val Req_Code: Int = 123
+    var firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Configure Google Sign In options
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        // getting the value of gso inside the GoogleSigninClient
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        // initialize the firebaseAuth variable
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.signInBtn.setOnClickListener { }
     }
