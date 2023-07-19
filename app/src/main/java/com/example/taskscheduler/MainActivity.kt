fd547a22bf7071d8b1e90a1739b455b4a91d034c
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-19 20:07:00 +0530

    created required variables

--- app/src/main/java/com/example/taskscheduler/MainActivity.kt
+++ app/src/main/java/com/example/taskscheduler/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.taskscheduler
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.taskscheduler.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code:Int=123
+    val firebaseAuth= FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.signInBtn.setOnClickListener {  }
+        binding.signInBtn.setOnClickListener { }
     }
 }
