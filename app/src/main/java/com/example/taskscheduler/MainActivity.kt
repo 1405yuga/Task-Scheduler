581c0346ffbf7eee20d3dfba785c7c591f04ea43
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-19 20:41:21 +0530

    getResult function implemented

--- app/src/main/java/com/example/taskscheduler/MainActivity.kt
+++ app/src/main/java/com/example/taskscheduler/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.taskscheduler
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taskscheduler.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -23,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         // Configure Google Sign In options
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestIdToken(getString(R.string.web_client_id))
             .requestEmail()
             .build()
         // getting the value of gso inside the GoogleSigninClient
@@ -31,6 +36,23 @@ class MainActivity : AppCompatActivity() {
         // initialize the firebaseAuth variable
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.signInBtn.setOnClickListener { }
+        binding.signInBtn.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    //create intent to display available google signinOptions
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK){
+                val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            }
+        }
+
+    private fun signInGoogle(){
+        val signInIntent : Intent = mGoogleSignInClient.signInIntent
+        getResult.launch(signInIntent)
+
     }
 }
