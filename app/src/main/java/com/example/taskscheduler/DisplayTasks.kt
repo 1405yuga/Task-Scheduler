6927fb2e4b166cd8ebcf24f17e9c1623f24200ce
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-20 16:22:45 +0530

    navigation View created

--- app/src/main/java/com/example/taskscheduler/DisplayTasks.kt
+++ app/src/main/java/com/example/taskscheduler/DisplayTasks.kt
@@ -33,7 +33,7 @@ class DisplayTasks : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.topAppBar.setNavigationOnClickListener {
-            //binding.drawerLayout.open()
+            binding.drawerLayout.open()
             Log.d(TAG, "navigation clicked")
         }
 
