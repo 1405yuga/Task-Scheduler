8f400dbddf00e0ee4e21b69730bfd0f6eb971e9a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-21 15:44:07 +0530

    created navHostFragment variable

--- app/src/main/java/com/example/taskscheduler/MainActivity.kt
+++ app/src/main/java/com/example/taskscheduler/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.taskscheduler
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.taskscheduler.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity tag"
@@ -9,11 +11,14 @@ private const val TAG = "MainActivity tag"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
     }
 
