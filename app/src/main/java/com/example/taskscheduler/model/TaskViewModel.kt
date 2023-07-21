2938a9458c16067ab175d5358d11e8554332a112
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-21 13:08:54 +0530

    created variables of user info

--- app/src/main/java/com/example/taskscheduler/model/TaskViewModel.kt
+++ app/src/main/java/com/example/taskscheduler/model/TaskViewModel.kt
@@ -6,6 +6,17 @@ import androidx.lifecycle.ViewModel
 
 class TaskViewModel : ViewModel() {
 
+
+    private var _userEmail = MutableLiveData<String>()
+    private var _userDisplayName = MutableLiveData<String>()
+    private var _userPhotoUri = MutableLiveData<String>()
+
+    val userEmail : LiveData<String> = _userEmail
+    val userDisplayName : LiveData<String> = _userDisplayName
+    val userPhotoUri : LiveData<String> = _userPhotoUri
+
     private val _tasksList = MutableLiveData<List<Task>>()
     val tasksList : LiveData<List<Task>> = _tasksList
+
+    // TODO: get tasksList
 }
