eb6cf620c04b22819abf68e7e26545f89695fc97
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-21 15:59:48 +0530

    created binding for SignUp Fragment

--- app/src/main/java/com/example/taskscheduler/SignUpFragment.kt
+++ app/src/main/java/com/example/taskscheduler/SignUpFragment.kt
@@ -5,21 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.taskscheduler.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater)
+        return binding.root
     }
 
 }
