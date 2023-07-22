7514b7d99ce3ac8f521a235ed84c41ea1ff14440
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-22 21:14:52 +0530

    created function to add task in firestore

--- app/src/main/java/com/example/taskscheduler/firebase/FirestoreFunctions.kt
+++ app/src/main/java/com/example/taskscheduler/firebase/FirestoreFunctions.kt
@@ -1,7 +1,26 @@
 package com.example.taskscheduler.firebase
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.example.taskscheduler.constants.ProjectConstants.USER_DEFAULT
+import com.example.taskscheduler.model.Task
 import com.google.firebase.firestore.FirebaseFirestore
+private const val TAG = "Firestore tag"
 
 object FirestoreFunctions {
 
+    fun addTask(user: String, task: Task,context: Context) {
+        if (user != USER_DEFAULT) {
+            val firestore = FirebaseFirestore.getInstance()
+            firestore.collection(user).add(task)
+                .addOnSuccessListener {
+                    Toast.makeText(context,"Task added",Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Log.d(TAG,it.message.toString())
+                    Toast.makeText(context,"Failed to add task",Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
 }
