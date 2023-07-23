0bd05e956a28f6c21a6e9a98bf9640ce3d5bd28a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-23 17:17:39 +0530

    timestamp field constant added

--- app/src/main/java/com/example/taskscheduler/constants/ProjectConstants.kt
+++ app/src/main/java/com/example/taskscheduler/constants/ProjectConstants.kt
@@ -3,4 +3,5 @@ package com.example.taskscheduler.constants
 object ProjectConstants {
     const val TASK_DEFAULT = "UNKNOWN TASK"
     const val USER_DEFAULT = "UNKNOWN USER"
+    const val TIMESTAMP = "timestamp"
 }
