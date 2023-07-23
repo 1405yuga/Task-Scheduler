f0556411cf5695e8b309fab380545b67c5c496a0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-23 17:19:58 +0530

    orderBy desc attached

--- app/src/main/java/com/example/taskscheduler/firebase/FirestoreFunctions.kt
+++ app/src/main/java/com/example/taskscheduler/firebase/FirestoreFunctions.kt
@@ -3,9 +3,11 @@ package com.example.taskscheduler.firebase
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import com.example.taskscheduler.constants.ProjectConstants.TIMESTAMP
 import com.example.taskscheduler.constants.ProjectConstants.USER_DEFAULT
 import com.example.taskscheduler.model.Task
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 private const val TAG = "Firestore tag"
 
@@ -28,17 +30,19 @@ object FirestoreFunctions {
     fun getTasks(user: String, context: Context) {
         if (user != USER_DEFAULT) {
             val firestore = FirebaseFirestore.getInstance()
-            firestore.collection(user).get()
+            firestore.collection(user)
+                .orderBy(TIMESTAMP, Query.Direction.DESCENDING)
+                .get()
                 .addOnSuccessListener { result ->
-                    for(doc in result){
-                        val task:Task = doc.toObject(Task::class.java)
+                    for (doc in result) {
+                        val task: Task = doc.toObject(Task::class.java)
                         Log.d(TAG, "${task.taskName} ${task.taskDetails} ${task.timestamp}")
                     }
 
                 }
                 .addOnFailureListener {
-                    Toast.makeText(context,"Failed to load task",Toast.LENGTH_SHORT).show()
-                    Log.d(TAG,it.message.toString())
+                    Toast.makeText(context, "Failed to load task", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, it.message.toString())
                 }
         }
     }
