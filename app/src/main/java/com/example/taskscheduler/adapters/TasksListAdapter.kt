8ff2f11bd80d6e98141349b715931338cc15add1
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-23 16:25:45 +0530

    created taskViewHolder

--- app/src/main/java/com/example/taskscheduler/adapters/TasksListAdapter.kt
+++ app/src/main/java/com/example/taskscheduler/adapters/TasksListAdapter.kt
@@ -1,6 +1,22 @@
 package com.example.taskscheduler.adapters
 
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taskscheduler.constants.TimeConvertingFunctions.convertTimestampToDateTime
+import com.example.taskscheduler.databinding.ListItemTaskBinding
 import com.example.taskscheduler.model.Task
 
-class TasksListAdapter(private val tasksList : List<Task>) {
+class TasksListAdapter(private val context : Context,private val tasksList : List<Task>) : RecyclerView.Adapter<TasksListAdapter.TaskViewHolder>() {
+
+    // holds the view
+    class TaskViewHolder(val binding : ListItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task : Task){
+            binding.apply {
+                taskName.text = task.taskName
+                details.text = task.taskDetails
+                dateTime.text = convertTimestampToDateTime(task.timestamp!!)
+            }
+        }
+    }
+
 }
