5ac3c7ee15826ddc352dd3a136b6d0ca630ded2d
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-24 12:58:16 +0530

    updating entire adapter

--- app/src/main/java/com/example/taskscheduler/adapters/TasksListAdapter.kt
+++ app/src/main/java/com/example/taskscheduler/adapters/TasksListAdapter.kt
@@ -1,44 +1,9 @@
 package com.example.taskscheduler.adapters
 
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.example.taskscheduler.constants.TimeConvertingFunctions.convertTimestampToDateTime
-import com.example.taskscheduler.databinding.ListItemTaskBinding
+import androidx.recyclerview.widget.ListAdapter
 import com.example.taskscheduler.model.Task
 
-class TasksListAdapter(private val context: Context, private val tasksList: List<Task>) :
-    RecyclerView.Adapter<TasksListAdapter.TaskViewHolder>() {
-
-    // holds the view
-    class TaskViewHolder(val binding: ListItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(task: Task) {
-            binding.apply {
-                taskName.text = task.taskName
-                details.text = task.taskDetails
-                val (dateValue, timeValue) = convertTimestampToDateTime(task.timestamp!!)
-                date.text = dateValue
-                time.text = timeValue
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding: ListItemTaskBinding =
-            ListItemTaskBinding.inflate(layoutInflater, parent, false)
-        return TaskViewHolder(binding)
-    }
-
-    override fun getItemCount(): Int {
-        return tasksList.size
-    }
-
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        val task: Task = tasksList.get(position)
-        holder.bind(task)
-    }
-
+class TasksListAdapter(private val updateList: (ArrayList<Task>) -> (Unit)) :
+    ListAdapter<Task, TasksListAdapter.TaskViewHolder>(DiffCallBack) {
 
 }
