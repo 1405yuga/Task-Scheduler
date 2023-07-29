a628cb0c3ec634fab5e3b003d23f0c1380f401ab
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-29 21:21:54 +0530

    created suspend function sto save preference

--- app/src/main/java/com/example/taskscheduler/DataStore/PreferencesDataStore.kt
+++ app/src/main/java/com/example/taskscheduler/DataStore/PreferencesDataStore.kt
@@ -3,11 +3,20 @@ package com.example.taskscheduler.DataStore
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 
 //instance of preference data store
 private val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = "layout_preferences")
 
 class PreferencesDataStore(context: Context) {
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
 
+    //save preferences
+    suspend fun saveLayoutPrefrence(context: Context,isLinearLayout : Boolean){
+        context.datastore.edit {mutablePreferences ->
+            mutablePreferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayout
+        }
+    }
 }
